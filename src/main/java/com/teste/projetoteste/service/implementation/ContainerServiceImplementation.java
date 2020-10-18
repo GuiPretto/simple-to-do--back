938aee45f7fbb680dca3af6c4599dde6d7710eca
Guilherme Cândido <guipretto5@gmail.com>
2020-10-18 15:59:38 -0300

    Creates Container service layer implementation

---
+++ src/main/java/com/teste/projetoteste/service/implementation/ContainerServiceImplementation.java
@@ -0,0 +1,74 @@
+package com.teste.projetoteste.service.implementation;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.teste.projetoteste.exception.ContainerException;
+import com.teste.projetoteste.model.entity.Card;
+import com.teste.projetoteste.model.entity.Container;
+import com.teste.projetoteste.model.repository.ContainerRepository;
+import com.teste.projetoteste.service.ContainerService;
+
+@Service
+public class ContainerServiceImplementation implements ContainerService {
+
+	private ContainerRepository repository;
+	
+	public ContainerServiceImplementation(ContainerRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	@Transactional
+	public Container createContainer(Container container) {
+		validateNewContainer(container);
+		return repository.save(container);
+	}
+	
+	@Override
+	@Transactional
+	public Container updateContainer(Container container) {
+		Objects.requireNonNull(container.getId());
+		validateExistentContainer(container);
+		return repository.save(container);
+	}
+
+	@Override
+	@Transactional
+	public void deleteContainer(Container container) {
+		Objects.requireNonNull(container.getId());
+		validateExistentContainer(container);
+		repository.delete(container);
+	}
+	
+	@Override
+	public void validateNewContainer(Container container) {
+		if (container.getTitle() == null || container.getTitle().trim().equals("")) {
+			throw new ContainerException("Digite um nome válido.");
+		}
+		boolean exists = repository.existsByTitle(container.getTitle());
+		if (exists) {
+			throw new ContainerException("Já existe um Grupo com este nome.");
+		}		
+	}
+
+	@Override
+	public void validateExistentContainer(Container container) {
+		if (container.getTitle() == null || container.getTitle().trim().equals("")) {
+			throw new ContainerException("Container com título inválido.");
+		}
+		boolean exists = repository.existsByTitle(container.getTitle());
+		if (!exists) {
+			throw new ContainerException("Este container não existe.");
+		}
+	}
+	
+	@Override
+	public Optional<Container> getById(int id) {
+		return repository.findById(id);
+	}		
+}
