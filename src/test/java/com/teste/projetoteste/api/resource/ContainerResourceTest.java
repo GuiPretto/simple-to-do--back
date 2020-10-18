177d1f3fb22c6a500f9939aa9c80cecffdd09ba3
Guilherme Cândido <guipretto5@gmail.com>
2020-10-18 16:06:46 -0300

    Creates Container controller layer tests

---
+++ src/test/java/com/teste/projetoteste/api/resource/ContainerResourceTest.java
@@ -0,0 +1,159 @@
+package com.teste.projetoteste.api.resource;
+
+import java.util.Optional;
+
+import javax.print.attribute.standard.Media;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teste.projetoteste.api.dto.ContainerDTO;
+import com.teste.projetoteste.exception.ContainerException;
+import com.teste.projetoteste.model.entity.Container;
+import com.teste.projetoteste.service.CardService;
+import com.teste.projetoteste.service.ContainerService;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers = ContainerResource.class)
+@AutoConfigureMockMvc
+public class ContainerResourceTest {
+
+	static final String API = "/api/containers";
+	static final MediaType JSON = MediaType.APPLICATION_JSON;
+	
+	@Autowired
+	MockMvc mvc;
+	
+	@MockBean
+	ContainerService service;
+	
+	@MockBean
+	CardService cardService;
+	
+	@Test
+	public void shouldCreateAContainer() throws Exception {
+		Container container = Container.builder().title("Teste").build();
+		ContainerDTO dto = ContainerDTO.builder().title("Teste").build();
+		Mockito.when(service.createContainer(Mockito.any(Container.class))).thenReturn(container);
+		String jsonData = new ObjectMapper().writeValueAsString(dto);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.post(API.concat("/create"))
+				.accept(JSON)
+				.contentType(JSON)
+				.content(jsonData);
+		
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isCreated())
+			.andExpect(MockMvcResultMatchers.jsonPath("title").value(container.getTitle()));	
+	}
+	
+	@Test
+	public void shouldReceiveABadRequestAfterTryingToCreateAContainer() throws Exception {
+		ContainerDTO dto = ContainerDTO.builder().title("Teste").build();
+		Mockito.when(service.createContainer(Mockito.any(Container.class))).thenThrow(ContainerException.class);
+		String jsonData = new ObjectMapper().writeValueAsString(dto);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.post(API.concat("/create"))
+				.accept(JSON)
+				.contentType(JSON)
+				.content(jsonData);
+		
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isBadRequest());
+	}
+	
+	@Test
+	public void shouldUpdateAContainer() throws Exception {
+		Container container = Container.builder().title("Teste").build();
+		ContainerDTO dto = ContainerDTO.builder().title("Teste").build();
+		Mockito.when(service.getById(Mockito.anyInt())).thenReturn(Optional.of(new Container()));
+		Mockito.when(service.updateContainer(Mockito.any(Container.class))).thenReturn(container);
+		String jsonData = new ObjectMapper().writeValueAsString(dto);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.put(API.concat("/update/2"))
+				.accept(JSON)
+				.contentType(JSON)
+				.content(jsonData);
+		
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("title").value(container.getTitle()));	
+	}
+	
+	@Test
+	public void shouldReceiveABadRequestAfterTryingToUpdateAContainer() throws Exception {
+		ContainerDTO dto = ContainerDTO.builder().title("Teste").build();
+		Mockito.when(service.getById(Mockito.anyInt())).thenReturn(Optional.of(new Container()));
+		Mockito.when(service.updateContainer(Mockito.any(Container.class))).thenThrow(ContainerException.class);
+		String jsonData = new ObjectMapper().writeValueAsString(dto);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.put(API.concat("/update/2"))
+				.accept(JSON)
+				.contentType(JSON)
+				.content(jsonData);
+		
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isBadRequest());
+	}
+	
+	@Test
+	public void shouldDeleteAContainer() throws Exception {
+		Container container = Container.builder().title("Teste").build();
+		ContainerDTO dto = ContainerDTO.builder().title("Teste").build();
+		Mockito.when(service.getById(Mockito.anyInt())).thenReturn(Optional.of(container));
+		Mockito.doNothing().when(service).deleteContainer(Mockito.any(Container.class));
+		String jsonData = new ObjectMapper().writeValueAsString(dto);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.delete(API.concat("/delete/2"))
+				.accept(JSON)
+				.contentType(JSON)
+				.content(jsonData);
+		
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isNoContent());	
+	}
+	
+	@Test
+	public void shouldReceiveABadRequestAfterTryingToDeleteAContainer() throws Exception {
+		Container container = Container.builder().title("Teste").build();
+		ContainerDTO dto = ContainerDTO.builder().title("Teste").build();
+		Mockito.when(service.getById(Mockito.anyInt())).thenReturn(Optional.of(container));
+		Mockito.doThrow(ContainerException.class).when(service).deleteContainer(Mockito.any(Container.class));
+		String jsonData = new ObjectMapper().writeValueAsString(dto);
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.delete(API.concat("/delete/2"))
+				.accept(JSON)
+				.contentType(JSON)
+				.content(jsonData);
+		
+		mvc
+			.perform(request)
+			.andExpect(MockMvcResultMatchers.status().isBadRequest());	
+	}
+}
