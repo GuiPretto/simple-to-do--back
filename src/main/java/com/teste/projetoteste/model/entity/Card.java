3012381fd773d747702dc9277799f9c004680834
Guilherme Cândido <guipretto5@gmail.com>
2020-10-18 15:37:54 -0300

    Creates Card entity class

---
+++ src/main/java/com/teste/projetoteste/model/entity/Card.java
@@ -0,0 +1,36 @@
+package com.teste.projetoteste.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "card", schema = "teste")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Card {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_container")
+	private Container container;
+}
