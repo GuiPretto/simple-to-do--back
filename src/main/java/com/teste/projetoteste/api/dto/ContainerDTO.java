b4ba91804cb567b6c1920be7b201e328c1b7da09
Guilherme Cândido <guipretto5@gmail.com>
2020-10-18 16:01:24 -0300

    Creates Container controller layer and his DTO

---
+++ src/main/java/com/teste/projetoteste/api/dto/ContainerDTO.java
@@ -0,0 +1,18 @@
+package com.teste.projetoteste.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContainerDTO {
+
+	private String title;
+	
+}
