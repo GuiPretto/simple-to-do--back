ee4601a7562b4b0604a82e1ef5e796e14d843a64
Guilherme Cândido <guipretto5@gmail.com>
2020-10-18 15:53:10 -0300

    Creates Card repository layer file

---
+++ src/main/java/com/teste/projetoteste/model/repository/CardRepository.java
@@ -0,0 +1,11 @@
+package com.teste.projetoteste.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teste.projetoteste.model.entity.Card;
+
+public interface CardRepository extends JpaRepository<Card, Integer> {
+	
+	boolean existsByTitle(String title);
+
+}
